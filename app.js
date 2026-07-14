8ff0b314bfa34da4b3e60dd650890b54e395a12e
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-07-14 19:00:37 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* ==========================================================================
-   AETHERIA E-Commerce JavaScript Logic 
+   AETHERIA E-Commerce JavaScript Logic
    ========================================================================== */
 
 // 1. Product Data Catalog (Level 3 Tasks)
