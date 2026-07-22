647e56237ff85db5645a3da03f91621be9726638
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-07-22 20:45:04 +0530

    update

--- app.js
+++ app.js
@@ -162,6 +162,7 @@ function initTheme() {
   document.documentElement.setAttribute("data-theme", savedTheme);
 }
 
+
 themeToggleBtn.addEventListener("click", () => {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
